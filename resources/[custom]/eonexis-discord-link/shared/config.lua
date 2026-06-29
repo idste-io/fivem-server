65f4c4cb7450b6e23da03383983d7aaea46b9cdc
Eonexis <invoxios@gmail.com>
2026-06-29 17:56:22 -0400

    fix(discord-link): set correct bot secret in config

--- resources/[custom]/eonexis-discord-link/shared/config.lua
+++ resources/[custom]/eonexis-discord-link/shared/config.lua
@@ -1,5 +1,5 @@
 Config = {}
--- Must match BOT_SECRET in /opt/eonexis-bot/.env
-Config.BotSecret    = 'change_this_to_a_random_32_char_string'
+-- Must match BOT_SECRET in /opt/eonexis-bot/.env on VPS
+Config.BotSecret    = 'eonexis_bot_secret_2024_xK9mQ'
 Config.CodeExpiry   = 600    -- seconds before /link code expires
 Config.BotHttpPort  = 3001   -- port the Discord bot HTTP server listens on
